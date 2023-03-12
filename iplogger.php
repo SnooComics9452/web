9fa0fa699a8f82662e3f1289770bf68c9ddf058c
SnooComics9452 <72796659+SnooComics9452@users.noreply.github.com>
2023-03-12 17:41:50 +0800

    Update iplogger.php

--- iplogger.php
+++ iplogger.php
@@ -8,7 +8,7 @@
 
 function logIP()
 {
-     $ipLog="logfile.htm"; // Your logfiles name here (.txt or .html extensions ok)
+     $ipLog="logfile.txt"; // Your logfiles name here (.txt or .html extensions ok)
 
      
 
