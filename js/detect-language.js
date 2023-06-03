4bd772be4829c80d0aedd89b3e0fa956db0b5a67
SnooComics9452 <72796659+SnooComics9452@users.noreply.github.com>
2023-06-04 01:49:55 +0800

    still breaks

--- js/detect-language.js
+++
@@ -1,8 +0,0 @@
-// Get the user's preferred language
-let userLanguage = navigator.language.toLowerCase();
-
-// Check if the user's language is Chinese
-if (userLanguage.indexOf("zh") === 0) {
-  // User's language is Chinese, redirect to the Chinese version of the page
-  window.location.href = "https://snoocomics9452.github.io/web/index.html";
-}
