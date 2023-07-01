6956090fdcaec93498b3c4e6d0e527c67175108c
SnooComics9452 <72796659+SnooComics9452@users.noreply.github.com>
2023-07-01 14:47:08 +0800

    .

--- test/js/time.js
+++ test/js/time.js
@@ -1,41 +1,26 @@
-const apiKey = 'e0eb840c0078ea69deed6696c2a4bd07';
+// Get the current time
+var date = new Date();
 
-getLocation();
+// Get the hours and minutes
+var hours = date.getHours();
+var minutes = date.getMinutes();
 
-function getLocation() {  
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showWeather);
-  } else {
-    // Geolocation not supported
-  }  
+// Convert hours from 24 to 12 hour format
+var timeFormat = "AM";
+if (hours > 12) {
+hours = hours - 12;
+timeFormat = "PM";
 }
 
-function showWeather(position) {
-  const { latitude, longitude } = position.coords;
-  
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`)
-    .then(res => res.json()) 
-    .then(data => {
-      const { timezone } = data;
-      const { main: weather } = data.weather[0];
-     
-      const time = formatTime(timezone);  
-      const weatherEl = document.getElementById('weather');  
-      weatherEl.textContent = weather;
-        
-      const timeEl = document.getElementById('time');   
-      timeEl.textContent = time;   
-    });
-}
+// Pad minutes with leading zero if needed
+minutes = minutes < 10 ? "0" + minutes : minutes;
 
-function formatTime(timezone) {
-  const offset = timezone / 60;
-  const date = new Date(new Date().getTime() + (offset * 1000));
-  
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  const period = hour >= 12 ? 'pm' : 'am';
-  const hour12 = hour % 12;
+// Concat hours, minutes and AM/PM
+var strTime = hours + ":" + minutes + " " + timeFormat;
 
-  return `${hour12}:${min} ${period}`  
-}
+// Display on website
+document.getElementById("time").innerHTML = strTime;
+
+Then in your HTML:
+
+<div id="time"></div>
